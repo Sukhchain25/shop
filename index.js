786cdbec5170285ab181450a3e52674938a8572b
Sukhchain Singh <sukhchain.singh@mylofamily.com>
2022-10-01 16:28:02 +0530

    jwt implementation and update user data route

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
 
 let port = process.env.PORT;
 dotenv.config();
@@ -12,7 +13,8 @@ mongoose
   .then(() => console.log("DB connection successfull"))
   .catch((err) => console.log(err));
 
-app.use(express.json())
+app.use(express.json());
+app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
 
 app.listen(port || 5000, () => {
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 //Authetication
 router.post("/register", async (req, res) => {
@@ -31,25 +32,25 @@ router.post("/register", async (req, res) => {
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ username: req.body.username });
-    !user &&
-      res.status(401).json({
-        success: false,
-        message: "Wrong credentials",
-      });
+    !user && res.status(401).json("Wrong credentials");
     const hashedPassword = CryptoJS.AES.decrypt(
       user.password,
       process.env.SECRET_KEY
     );
-    const password = hashedPassword.toString(CryptoJS.enc.Utf8);
-    password !== req.body.password &&
-      res.status(201).json({
-        success: false,
-        message: "Wrong credentials",
-      });
-    res.status(200).json({
-      success: true,
-      user: user,
-    });
+    const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+    OriginalPassword !== req.body.password &&
+      res.status(201).json("Wrong credentials");
+
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "3d" }
+    );
+    const { password, ...others } = user._doc;
+    res.status(200).json({ ...others, accessToken });
   } catch (err) {
     res.status(500).json(err);
   }
